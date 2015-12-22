e53fe4fb11b61362e0913f52cb977400312a06a4
Joigca <joseigualda@gmail.com>
2015-12-22 16:54:08 +0100

    Paso4

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,7 @@
 var confDB = {    
     existe_db:"",
     db:"",
-    initialize:function(){
+    initialize:function(tx){
         this.existe_db = window.localStorage.getItem("existe_db");
 
         //Creacion de la base de datos
@@ -29,6 +29,8 @@ var confDB = {
         if(this.existe_db == null){
             console.log("No existe base de datos");
             this.createDB();
+        }else{
+            cargarDB.initialize();
         }
     },
 
@@ -42,27 +44,82 @@ var confDB = {
     createCRMDB:function(tx){
         console.log("Creando tabla...");
 
-        var sql = "CREATE TABLE IF NOT EXISTS CRMDB(id INTEGER PRIMARY KEY AUTOINCREMENT, nombreCompleto VARCHAR(45), rol VARCHAR(45),ciudad VARCHAR(45), edad INTEGER, twitter BOOLEAN, facebook BOOLEAN, google BOOLEAN, correoGmail VARCHAR(45), correoHotmail VARCHAR(45),telefono VARCHAR(9), ultimos INTEGER)";
+        var sql = "CREATE TABLE IF NOT EXISTS CRMDB(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre VARCHAR(45), rol VARCHAR(45),ciudad VARCHAR(45), edad INTEGER, twitter BOOLEAN, facebook BOOLEAN, google BOOLEAN, correoGmail VARCHAR(45), correoHotmail VARCHAR(45),telefono VARCHAR(9), ultimos INTEGER)";
 
         tx.executeSql(sql);
 
         //INSERCION DE DATOS
 
-        sql = "INSERT INTO CRMDB(id, nombreCompleto, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (1, 'Jose Igualada', 'Estudiante', 'Torrent', 21, 1, 1, 1, 'joseigualada@gmail.com', 'joseigualada@hotmail.com', '676170946', '0')";
+        sql = "INSERT INTO CRMDB(id, nombre, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (1, 'Jose Igualada', 'Estudiante', 'Torrent', 21, 1, 1, 1, 'joseigualada@gmail.com', 'joseigualada@hotmail.com', '676170946', '0')";
+        tx.executeSql(sql);
+
+        sql = "INSERT INTO CRMDB(id, nombre, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (2, 'Joaquin Bahamonde', 'Estudiante', 'Catarroja', 21, 1, 1, 1, 'joaquinbahamonde@gmail.com', 'joaquinbahamonde@hotmail.com', '676170946', '0')";
+        tx.executeSql(sql);
+
+        sql = "INSERT INTO CRMDB(id, nombre, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (3, 'Ivan Estruch', 'Estudiante', 'Silla', 20, 1, 1, 1, 'ivanestruch@gmail.com', 'ivanestruch@hotmail.com', '676170946', '1')";
+        tx.executeSql(sql);
+
+        sql = "INSERT INTO CRMDB(id, nombre, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (4, 'Adrian Rodriguez', 'Estudiante', 'Alfafar', 19, 1, 1, 1, 'adrianrodriguez@gmail.com', 'adrianrodriguez@hotmail.com', '676170946', '1')";
+        tx.executeSql(sql);
+
+        sql = "INSERT INTO CRMDB(id, nombre, rol, ciudad, edad, twitter, facebook, google, correoGmail, correoHotmail, telefono, ultimos) VALUES (5, 'Silvia Reolid', 'Profesora', 'Valencia', 30, 1, 1, 1, 'silviareolid@gmail.com', 'silviareolid@hotmail.com', '676170946', '1')";
         tx.executeSql(sql);
 
     },
 
     createDBError:function(err){
-        console.log("**********************************************************************************")
         console.log("No se puede crear la Base de Datos: " + err.message);
     },
 
     createDBSucc:function(){
         console.log("La Base de Datos ha sido creada con exito");
         window.localStorage.setItem("existe_db", 1);
+        cargarDB.initialize()
     },
 };
+
+ var cargarDB = {
+     db:"",
+
+     initialize: function(){
+         //Leemos la BD
+         this.db = window.openDatabase("CRMDB", "1.0", "Base de datos de MiniCRM", 2*1024*1024);
+         this.cargaDB();
+     },
+
+     cargaDB: function(){
+         console.log("Cargando la Base de Datos");
+         //transaccion
+         this.db.transaction(this.mostrarCRMDB, this.mostrarDBError);
+     },
+
+     mostrarCRMDB:function(tx){
+
+         var sql = "SELECT * FROM CRMDB ORDER BY ultimos DESC;";
+         tx.executeSql(sql, [],
+
+             function(tx, result){
+
+                 if(result.rows.length>0){
+
+                     for(var i=0;i<result.rows.length;i++){
+                         var fila = result.rows.item(i);
+                         console.log("Fila --> "+i+" Nombre: "+fila.nombre);
+                     }
+
+                 }
+
+             },
+
+             function(tx, err){
+                 console.log("Ha ocurrido un error"+err.message)
+             }
+
+         );
+
+     },
+
+ }
 
 var app = {
     // Application Constructor
